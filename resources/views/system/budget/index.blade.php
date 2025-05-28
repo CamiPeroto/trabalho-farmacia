0e48b1a5b88db55771009c2e539a6c8a32a01cf1
CamiPeroto <camilabuenoperoto@gmail.com>
2025-05-28 19:15:01 -0300

    feat: tela cadastrar principio ativo

--- resources/views/system/budget/index.blade.php
+++ resources/views/system/budget/index.blade.php
@@ -93,69 +93,6 @@
             <div class="swiper-button-next"></div>
             <div class="swiper-button-prev"></div>
         </div>
-        {{-- <div class="container d-flex">
-            <div class="col-3">
-                <div class="card shadow" style="width: 21rem;">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                    </div>
-                    <img src="https://dmvfarma.vtexassets.com/arquivos/ids/271865/DIPIRONA%20500MG%20C20.png.png?v=638827583361130000"
-                        class="card-img-top" alt="..." style="width: 100%;">
-                    <div class="card-body d-flex align-items-end gap-3">
-                        <p class="fw-bold mb-0" id="p-price">R$:5.00/unt</p>
-                        <ul class="list-group list-group-flush">
-                            <li class="list-group-item rounded">Comprar</li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-            <div class="col-3">
-                <div class="card shadow" style="width: 21rem;">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                    </div>
-                    <img src="https://airela.com.br/wp-content/uploads/2024/02/ibuprofeno_400mg_ibuprofeno_10capsulas.png"
-                        class="card-img-top" alt="..." style="width: 80%;">
-                    <div class="card-body d-flex  align-items-end gap-3">
-                        <p class="fw-bold" id="p-price">R$:5.00/unt</p>
-                        <ul class="list-group list-group-flush">
-                            <li class="list-group-item rounded">Comprar</li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-            <div class="col-3">
-                <div class="card shadow" style="width: 21rem;">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                    </div>
-                    <img src="https://dmvfarma.vtexassets.com/arquivos/ids/271865/DIPIRONA%20500MG%20C20.png.png?v=638827583361130000"
-                        class="card-img-top" alt="...">
-                    <div class="card-body d-flex  align-items-end gap-3">
-                        <p class="fw-bold" id="p-price">R$:5.00/unt</p>
-                        <ul class="list-group list-group-flush">
-                            <li class="list-group-item rounded">Comprar</li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-            <div class="col-3">
-                <div class="card shadow" style="width: 21rem;">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                    </div>
-                    <img src="https://airela.com.br/wp-content/uploads/2024/02/ibuprofeno_400mg_ibuprofeno_10capsulas.png"
-                        class="card-img-top" alt="..." style="width: 80%;">
-
-                    <div class="card-body d-flex  align-items-end gap-3">
-                        <p class="fw-bold" id="p-price">R$:5.00/unt</p>
-                        <ul class="list-group list-group-flush">
-                            <li class="list-group-item rounded">Comprar</li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div> --}}
     </div>
 @endsection
 @section('javascript')
